6b907fa3acd5ef2dd69d8696698cd5103a6e657b
Abdulaziz Sharipov <dildorkomil@gmail.com>
2025-05-22 17:04:14 +0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,14 +1,13 @@
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-# ReplyKeyboardMarkup - Это класс для создания области под кнопки
-# KeyboardButton - Это класс для создания кнопки
+
 
 
 def menu_buttons():
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)  # Создали область под кнопки
-    btn1 = KeyboardButton(text='Фото дня 🌌')  # Создали Кнопку
-    btn2 = KeyboardButton(text='Интересный факт 🪐')  # Создали Кнопку
-    btn3 = KeyboardButton(text='Где сейчас МКС?')  # Создали Кнопку
-    btn4 = KeyboardButton(text='Астероиды рядом ☄')  # Создали Кнопку
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)  
+    btn1 = KeyboardButton(text='Фото дня 🌌') 
+    btn2 = KeyboardButton(text='Интересный факт 🪐')  
+    btn3 = KeyboardButton(text='Где сейчас МКС?') 
+    btn4 = KeyboardButton(text='Астероиды рядом ☄') 
     btn5 = KeyboardButton(text='Restart 🔁')
     markup.add(btn1, btn2, btn3, btn4, btn5)
     return markup
